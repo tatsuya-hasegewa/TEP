24666b9c519d002522c201b09b16a7cc17e9038b
twinkletia <vamosidemochumjetze215@gmail.com>
2022-12-19 00:06:34 +0900

    fix UART serial controller, add serial simulation

--- serial.h
+++ serial.h
@@ -1,17 +1,18 @@
-declare serial_in {
- input rxd;
- output data[8];
- func_in port_read;
- func_out rxready;
- func_in baud;
-}
-declare serial_out {
- output txd;
- input data[8];
- func_out txbusy;
- func_in baud;
- func_in port_write(data);
-}
-declare serial_clkfix {
- func_out baud;
+
+// 50MHz
+#define BAUD_9600 651
+#define BAUD_38400 162
+#define BAUD_115200 54
+#define BAUD_CNT BAUD_38400
+
+// 25MHz
+// #define BAUD_CNT (BAUD_38400 / 2)
+
+// 12.5MHz
+// #define BAUD_CNT (BAUD_38400 / 4)
+
+declare serial_clkfix
+{
+    func_out baud;
+    func_in sync;
 }
--- sysmain.v
+++ sysmain.v
@@ -3,7 +3,7 @@ parameter STEP=10;
 integer i,j,vcd,conindex;
 input p_reset, m_clock;
 wire p_reset, m_clock;
-reg int_signal;	//Š„‚è‚İƒŒƒWƒXƒ^
+reg int_signal;	//å‰²ã‚Šè¾¼ã¿ãƒ¬ã‚¸ã‚¹ã‚¿
 reg [7:0] mem [0:4091];
 reg [7:0] conin [0:255];
 reg [15:0] datai;
@@ -28,36 +28,30 @@ wire [7:0] led;
 wire [3:0] an; 
 wire [7:0] sseg; 
 sys sys(.p_reset(p_reset),.m_clock(m_clock),.PS2C(PS2C),.PS2D(PS2D),.RXD(RXD),.btn(btn),.VGA_V(VGA_V),.VGA_H(VGA_H),.VGA_B(VGA_B),.VGA_G(VGA_G),.VGA_R(VGA_R),.TXD(TXD),.led(led),.an(an),.sseg(sseg),.sw(sw));
-
+wire sim_rxd;
+wire done;
+wire rxready;
+wire port_read;
+wire [7:0] data;
+serial_in sim_serrx (.p_reset( p_reset), .m_clock(m_clock), .rxd(sim_rxd), .rxready(rxready), .port_read(port_read), .data(data), .done(done));
 assign wb = sys.cpu.wb;
 assign hlt = sys.cpu.hlt;
-
+assign sim_rxd = p_reset ? 1 : TXD;
 
 //always #(STEP/2) m_clock=~m_clock;
-//‚±‚±‚ÅŠ„‚è‚İ‚ÌON,OFF‚ğİ’è‚·‚éB
-always #(STEP) int_signal=1;			//Š„‚è‚İƒŒƒWƒXƒ^‚Ì’l
-
+//ã“ã“ã§å‰²ã‚Šè¾¼ã¿ã®ON,OFFã‚’è¨­å®šã™ã‚‹ã€‚
+always #(STEP) int_signal=1;			//å‰²ã‚Šè¾¼ã¿ãƒ¬ã‚¸ã‚¹ã‚¿ã®å€¤
 
 always @(negedge sys.cpu.m_clock)
 begin
-   sys.sertx.txbusyr = 0;
-   if( sys.serrx.rxreadyr == 0)
-    begin
-    if( conin[conindex] != 0)
-	   begin
-		   #(STEP)
-	    sys.serrx.rxd1 = conin[conindex]; 
-	    conindex = conindex + 1;
-	    sys.serrx.rxreadyr = 1;
-	   end
-    else
-    if( conin[conindex] == 0) $finish;
-    end
+   if(sim_serrx.done)
+	begin
+	$write("%c",data);
+	end
 end
-
 always @(negedge sys.cpu.m_clock)
 begin
- 
+/*
 if(wb)
 
  #(STEP)
@@ -135,10 +129,10 @@ if(wb)
 	endcase
 	//endcase  
   end
-	
+*/
 if(hlt)
   begin 
-  $display("pc:%x HLT   OP :%b %b %b %b\n  R01:%x R02:%x R03:%x R04:%x R05:%x R06:%x R07:%x R08:%x R09:%x R10:%x R11:%x R12:%x R13:%x R14:%x R15:%x I:%x"
+  $display("\npc:%x HLT   OP :%b %b %b %b\n  R01:%x R02:%x R03:%x R04:%x R05:%x R06:%x R07:%x R08:%x R09:%x R10:%x R11:%x R12:%x R13:%x R14:%x R15:%x I:%x"
 		 			,sys.cpu.pc,sys.cpu.opreg[15:12],sys.cpu.opreg[11:8],sys.cpu.opreg[7:4],sys.cpu.opreg[3:0], sys.cpu.rf.r[01], sys.cpu.rf.r[02], sys.cpu.rf.r[03], sys.cpu.rf.r[04], sys.cpu.rf.r[05], sys.cpu.rf.r[06], sys.cpu.rf.r[07], sys.cpu.rf.r[08], sys.cpu.rf.r[09], sys.cpu.rf.r[10], sys.cpu.rf.r[11], sys.cpu.rf.r[12], sys.cpu.rf.r[13], sys.cpu.rf.r[14], sys.cpu.rf.r[15], sys.cpu.I);
 
   $display("\nHALTED at %8d clock", $time/STEP);
@@ -156,11 +150,7 @@ end
 
 
 
-
-initial begin		//‰Šú‰»
- for(i=0; i<255; i=i+1) conin[i] = 0;
- $readmemh("consol.hex", conin);
- conindex = 0;
+initial begin		//åˆæœŸåŒ–
  $readmemh("tep.mem", mem);
  for(i=0; i<4096; i=i+1)
  begin
@@ -172,17 +162,10 @@ initial begin		//‰Šú‰»
 	 sys.mainmem.rame.ram[i>>1] = mem[i];
 	end
  end
- if($value$plusargs("vcd=%d",vcd)) begin
-   $dumpfile("tep.vcd");
-   $dumpvars(0,sys);
-   end
- //m_clock=0;
- //p_reset=1;
- int_signal=0;		//Š„‚è‚İƒŒƒWƒXƒ^
- //#(STEP) p_reset=0;
+ int_signal=0;		//å‰²ã‚Šè¾¼ã¿ãƒ¬ã‚¸ã‚¹ã‚¿
  sys.interval = 'hfffc;
 
- #(60000*STEP+(STEP/2)) $finish;
+#(10000*STEP+(STEP/2)) $finish;
 end
 endmodule
 
