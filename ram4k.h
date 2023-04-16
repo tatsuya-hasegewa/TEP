569855863745ef703e94eb2454398c0b2520ce28
twinkletia <vamosidemochumjetze215@gmail.com>
2023-04-16 13:11:55 +0900

    expand region

--- ram4k.h
+++ ram4k.h
@@ -1,5 +1,5 @@
 #define WIDTH 16
-#define DEPTH 2048
+#define DEPTH 16384
 #define ADDR_MSB (_int(_log10(_real(DEPTH * (WIDTH / 8))) / _log10(_real(2))) - 1)
 #define ADDR_LSB (_int(_log10(_real(WIDTH)) / _log10(_real(2))) - _int(_log10(_real(8)) / _log10(_real(2))))
 #define LENGTH (ADDR_MSB - ADDR_LSB + 1)
