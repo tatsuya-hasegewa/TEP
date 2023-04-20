f437060691e888b728bf32c8030220cea0e25c23
twinkletia <vamosidemochumjetze215@gmail.com>
2023-04-20 12:30:43 +0900

    シミュ実行トレースログの追加

--- sysmain.v
+++ sysmain.v
@@ -1,8 +1,10 @@
-module main(p_reset,m_clock);
+module main(p_reset,m_clock,wb);
 parameter STEP=10;
 integer i,j,vcd,conindex;
 input p_reset, m_clock;
 wire p_reset, m_clock;
+output wb;
+wire wb;
 reg int_signal;	//割り込みレジスタ
 reg [7:0] mem [0:4091];
 reg [7:0] conin [0:255];
@@ -10,7 +12,7 @@ reg [15:0] datai;
 
 wire [15:0]	datao,adrs;
 
-wire	memory_read,memory_write,memory_write_byte,wb,hlt;
+wire	memory_read,memory_write,memory_write_byte,hlt;
 wire	txd,in3,baudin;
 
 reg PS2C; 
@@ -34,6 +36,7 @@ wire rxready;
 wire port_read;
 wire [7:0] data;
 serial_in sim_serrx (.p_reset( p_reset), .m_clock(m_clock), .rxd(sim_rxd), .rxready(rxready), .port_read(port_read), .data(data), .done(done));
+
 assign wb = sys.cpu.wb;
 assign hlt = sys.cpu.hlt;
 assign sim_rxd = p_reset ? 1 : TXD;
@@ -51,85 +54,8 @@ begin
 end
 always @(negedge sys.cpu.m_clock)
 begin
-if(wb)
 
  #(STEP)
-/*
-  begin
-  $write("pc:%x ",sys.cpu.pc);
-  case (sys.cpu.opreg[15])
-  'b0:
-   begin
-	case (sys.cpu.opreg[14:8])	// Itype
-	'b0000000: $write("CNST ");
-	'b0000001: $write("LD1  ");
-	'b0000010: $write("LD2  ");
-	'b0000011: $write("ST1  ");
-	'b0000100: $write("ST2  ");
-	'b0000101: $write("JUMP ");
-	'b0000110: $write("CALL ");
-	'b0000111: $write("JEQ  ");
-	'b0001000: $write("JGEI ");
-	'b0001001: $write("JGEU ");
-	'b0001010: $write("JGTI ");
-	'b0001011: $write("JGTU ");
-	'b0001100: $write("JLEI ");
-	'b0001101: $write("JLEU ");
-	'b0001110: $write("JLTI ");
-	'b0001111: $write("JLTU ");
-	'b0010000: $write("JNE  ");
-	endcase
-	$display(" OP1:%b %b %b %b OP2:%b %b %b %b \n  R01:%x R02:%x R03:%x R04:%x R05:%x R06:%x R07:%x R08:%x R09:%x R10:%x R11:%x R12:%x R13:%x R14:%x R15:%x I:%x"
-	,sys.cpu.opreg[15:12],
-	sys.cpu.opreg[11:8], sys.cpu.opreg[7:4], sys.cpu.opreg[3:0],
-	sys.cpu.I[15:12], sys.cpu.I[11:8], sys.cpu.I[7:4], sys.cpu.I[3:0], 
-	sys.cpu.rf.r[01], sys.cpu.rf.r[02], sys.cpu.rf.r[03], sys.cpu.rf.r[04], 
-	sys.cpu.rf.r[05], sys.cpu.rf.r[06], sys.cpu.rf.r[07], sys.cpu.rf.r[08], 
-	sys.cpu.rf.r[09], sys.cpu.rf.r[10], sys.cpu.rf.r[11], sys.cpu.rf.r[12], 
-	sys.cpu.rf.r[13], sys.cpu.rf.r[14], sys.cpu.rf.r[15], sys.cpu.I);
-   end
-'b1:
-   begin
-   case (sys.cpu.opreg[14]) 		// Rtype or RStype
-	'b0:
-        begin
-           case (sys.cpu.opreg[13:8]) 		// Rtype
-		'b000000: $write("MUL  ");
-		'b000001: $write("CVI2 ");
-		'b000010: $write("ADD  ");
-		'b000011: $write("SUB  ");
-		'b000100: $write("NEG  ");
-		'b000101: $write("BAND ");
-		'b000110: $write("BOR  ");
-		'b000111: $write("BXOR ");
-		'b001000: $write("BCOM ");
-		'b101001: $write("DINT ");
-		'b101010: $write("EINT ");
-		'b101011: $write("RINT ");
-	endcase
-	$display("  OP :%b %b %b %b\n  R01:%x R02:%x R03:%x R04:%x R05:%x R06:%x R07:%x R08:%x R09:%x R10:%x R11:%x R12:%x R13:%x R14:%x R15:%x I:%x"
-	,sys.cpu.opreg[15:12],sys.cpu.opreg[11:8],sys.cpu.opreg[7:4],sys.cpu.opreg[3:0],
-	sys.cpu.rf.r[01], sys.cpu.rf.r[02], sys.cpu.rf.r[03], sys.cpu.rf.r[04],
-	sys.cpu.rf.r[05], sys.cpu.rf.r[06], sys.cpu.rf.r[07], sys.cpu.rf.r[08],
-	sys.cpu.rf.r[09], sys.cpu.rf.r[10], sys.cpu.rf.r[11], sys.cpu.rf.r[12],
-	sys.cpu.rf.r[13], sys.cpu.rf.r[14], sys.cpu.rf.r[15], sys.cpu.I);
-        end
-  'b1:begin
-      case (sys.cpu.opreg[13:12]) 		//  RStype
-	'b00: $write("LSHL ");
-	'b01: $write("RSHA ");
-	'b10: $write("RSHL ");
-	'b11: $write("HLT  ");
-      endcase
-	$display("OP :%b %b %b %b  \n  R01:%x R02:%x R03:%x R04:%x R05:%x R06:%x R07:%x R08:%x R09:%x R10:%x R11:%x R12:%x R13:%x R14:%x R15:%x I:%x"
-	,sys.cpu.opreg[15:12],sys.cpu.opreg[11:8],sys.cpu.opreg[7:4],sys.cpu.opreg[3:0], sys.cpu.rf.r[01], sys.cpu.rf.r[02], sys.cpu.rf.r[03], sys.cpu.rf.r[04], sys.cpu.rf.r[05], sys.cpu.rf.r[06], sys.cpu.rf.r[07], sys.cpu.rf.r[08], sys.cpu.rf.r[09], sys.cpu.rf.r[10], sys.cpu.rf.r[11], sys.cpu.rf.r[12], sys.cpu.rf.r[13], sys.cpu.rf.r[14], sys.cpu.rf.r[15], sys.cpu.I);
-	  end
-	endcase
-	  end
-	endcase
-	//endcase  
-  end
-*/
 if(hlt)
   begin 
   $display("\npc:%x HLT   OP :%b %b %b %b\n  R01:%x R02:%x R03:%x R04:%x R05:%x R06:%x R07:%x R08:%x R09:%x R10:%x R11:%x R12:%x R13:%x R14:%x R15:%x I:%x"
--- testBench.cpp
+++ testBench.cpp
@@ -8,9 +8,43 @@
 
 uint64_t main_time = 0;
 
+char *Iinst[] = {"CNST", "LD1", "LD2", "ST1", "ST2", "JUMP", "CALL", "JEQ", "JGEI", "JGEU", "JGTI", "JGTU", "JLEI", "JLEU", "JLTI", "JLTU", "JNE"};
+char *Rinst[] = {"MUL", "CVI2", "ADD", "SUB", "NEG", "BAND", "BOR", "BXOR", "BCOM", "DINT", "EINT", "RINT"};
+char *RSinst[] = {"LSHL", "RSHA", "RSHL", "HLT"};
+
 double sc_time_stamp()
 {
     return main_time;
+}
+
+unsigned int bitTest(unsigned int x, int bit_position)
+{
+    // テスト対象のビット位置を 1 にセットしたマスクを作成する
+    unsigned int mask = 1 << bit_position;
+
+    // x とマスクを AND 演算することで、テスト対象のビット値を取得する
+    unsigned int bit_value = x & mask;
+
+    // テスト対象のビット値が 0 でなければ、ビットがセットされていることになる
+    if (bit_value != 0)
+    {
+        return 1; // ビットがセットされている場合は 1 を返す
+    }
+    else
+    {
+        return 0; // ビットがセットされていない場合は 0 を返す
+    }
+}
+
+unsigned int bitExtract(unsigned int x, int msb, int lsb)
+{
+    // 計算に使用するマスクを作成する
+    unsigned int mask = ((1 << (msb - lsb + 1)) - 1) << lsb;
+
+    // x の該当するビット範囲を取り出す
+    unsigned int extracted_bits = (x & mask) >> lsb;
+
+    return extracted_bits;
 }
 
 class Test
@@ -18,6 +52,7 @@ class Test
 private:
     Vmain *top;
     VerilatedVcdC *tfp;
+    FILE *logfile = NULL;
     int reset = 0;
     int p_reset = 0;
 
@@ -35,6 +70,10 @@ public:
         if (tfp->isOpen())
         {
             tfp->close();
+        }
+        if (logfile)
+        {
+            fclose(logfile);
         }
         top->final();
     };
@@ -55,26 +94,43 @@ public:
             tfp->dump(m_clock_count);
         }
     };
+    void logging(void)
+    {
+        if (top->m_clock && top->wb && logfile)
+        {
+            disasmTEP();
+        }
+    }
     void step(void)
     {
         tick();
         eval();
         dump();
+        logging();
     };
     void parseLogOpts(int argc, char **argv)
     {
+        FILE *fp;
         int opt, longindex;
         char *Darg = NULL, *aarg = NULL, *symfile = NULL;
         struct option longopts[] = {
             {"dump", no_argument, NULL, 'd'},
+            {"log", no_argument, NULL, 'l'},
             {0, 0, 0, 0},
         };
-        while ((opt = getopt_long(argc, argv, "d", longopts, &longindex)) != -1)
+        while ((opt = getopt_long(argc, argv, "dl", longopts, &longindex)) != -1)
         {
             switch (opt)
             {
             case 'd':
                 tfp->open("tep.vcd");
+                break;
+            case 'l':
+                fp = fopen("tep.log", "w");
+                if (fp)
+                {
+                    logfile = fp;
+                }
                 break;
             default:
                 exit(1);
@@ -103,6 +159,38 @@ public:
             dump();
         }
     };
+    void disasmTEP()
+    {
+        uint16_t opcode, pc, r1, r1_val, r2, r2_val, imm, shiftval, alu_out;
+        char *inst;
+        VlUnpacked<SData /*15:0*/, 16> *regs;
+        pc = top->rootp->main__DOT__sys__DOT__cpu__DOT__pc;
+        pc -= 2; // writeback時は素手にカウント済みなので戻す
+        opcode = top->rootp->main__DOT__sys__DOT__cpu__DOT__opreg;
+        imm = top->rootp->main__DOT__sys__DOT__cpu__DOT__I;
+        regs = &top->rootp->main__DOT__sys__DOT__cpu__DOT__rf__DOT__r;
+        alu_out = top->rootp->main__DOT__sys__DOT__cpu__DOT___alu_out;
+        r1 = bitExtract(opcode, 7, 4);
+        r2 = bitExtract(opcode, 3, 0);
+        r1_val = (*regs)[r1];
+        r2_val = (*regs)[r2];
+        if (!bitTest(opcode, 15)) // Itype
+        {
+            inst = Iinst[bitExtract(opcode, 14, 8)];
+            fprintf(logfile, "%04X: %-5s$%02d,$%02d,%04x\t\t$%02d=$%04X,\t$%02d=$%04X,\taluOUT=%04X\n", pc, inst, r2, r1, imm, r2, r2_val, r1, r1_val, alu_out);
+        }
+        else if (!bitTest(opcode, 14))
+        {                                            // Rtype
+            inst = Rinst[bitExtract(opcode, 11, 8)]; // 本来instは13:8だが、DINT,EINT,RINTをRinstのインデクスと対応付けるために切り出し範囲を縮小
+            fprintf(logfile, "%04X: %-5s$%02d,$%02d%*s\t\t$%02d=$%04X,\t$%02d=$%04X,\taluOUT=%04X\n", pc, inst, r2, r1, 5, "", r2, r2_val, r1, r1_val, alu_out);
+        }
+        else
+        { // RStype
+            shiftval = (*regs)[bitExtract(opcode, 11, 8)];
+            inst = RSinst[bitExtract(opcode, 13, 12)];
+            fprintf(logfile, "%04X: %-5s$%02d,$%02d,%04x\t\t$%02d=$%04X,\t$%02d=$%04X,\taluOUT=%04X\n", pc, inst, r2, r1, shiftval, r2, r2_val, r1, r1_val, alu_out);
+        }
+    }
 };
 
 int main(int argc, char **argv)
